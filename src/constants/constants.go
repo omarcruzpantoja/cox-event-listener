dada9855210be95c9ca5c16caa28b1e3abc0f8ff
Omar Cruz Pantoja <opantoja3@gatech.edu>
2026-01-11 13:49:39 -0500

    chore(constants): add blackhollow server ids

--- src/constants/constants.go
+++ src/constants/constants.go
@@ -22,8 +22,9 @@ type RelayMessageIDs struct {
 }
 
 var (
-	ROGUE_SERVER_ID = "1094994806511517776"
-	PO_SERVER_ID    = "1325993203836518431"
+	ROGUE_SERVER_ID        = "1094994806511517776"
+	BLACK_HOLLOW_SERVER_ID = "1459735738953240712"
+	PO_SERVER_ID           = "1325993203836518431"
 	// Whenever you define a message to assign roles to users, you'd
 	// set the message ID here. So say you have a "React on emoji X for
 	// Y role", the id of the message that contains those reactions will be added
@@ -36,7 +37,10 @@ var (
 	ASSIGN_ROLE_MESSAGE_IDS = []string{
 		"1415116596392890368", // P&O Server Rates
 		"1415141373392453702", // P&O Server Lab Boss Spawn
-		"1419148638071947405", // Specific Lab Boss Spawn
+		"1419148638071947405", // P&O Specific Lab Boss Spawn
+		"1459981084526776607", // BlackHollow Server Rates
+		"1459979919504183461", // BlackHollow Server Lab Boss Spawn
+		"1459979979210362901", // BlackHollow Specific Lab Boss Spawn
 	}
 
 	// This is used to identify which channels to listen events. This is for your
@@ -59,61 +63,74 @@ var (
 
 	// Channels to send messages to, this is the channel you create for mentions to be sent.
 	RELAY_MESSAGE_CHANNEL_IDS = []RelayMessageIDs{
-		{ServerID: PO_SERVER_ID, ChannelID: "1408930128205053972"},    // P&O events feed channel
-		{ServerID: ROGUE_SERVER_ID, ChannelID: "1415864173367398441"}, // ROGUE CHANNEL
+		{ServerID: PO_SERVER_ID, ChannelID: "1408930128205053972"},           // P&O events feed channel
+		{ServerID: ROGUE_SERVER_ID, ChannelID: "1415864173367398441"},        // ROGUE CHANNEL
+		{ServerID: BLACK_HOLLOW_SERVER_ID, ChannelID: "1459923502982365488"}, // BLACK HOLLOW CHANNEL
 	}
 
 	// Role IDs for `Drop rate all`
 	DROP_RATE_ROLE_IDS = []RoleIDs{
-		{ServerID: PO_SERVER_ID, RoleID: "1408913591125540954"},    // P&O server
-		{ServerID: ROGUE_SERVER_ID, RoleID: "1387734426502565969"}, // ROGUE server
+		{ServerID: PO_SERVER_ID, RoleID: "1408913591125540954"},           // P&O server
+		{ServerID: ROGUE_SERVER_ID, RoleID: "1387734426502565969"},        // ROGUE server
+		{ServerID: BLACK_HOLLOW_SERVER_ID, RoleID: "1459977383032524942"}, // BLACK HOLLOW server
 	}
 
 	DROP_RATE_10X_ROLE_IDS = []RoleIDs{
-		{ServerID: PO_SERVER_ID, RoleID: "1408913379137032322"}, // P&O server
+		{ServerID: PO_SERVER_ID, RoleID: "1408913379137032322"},           // P&O server
+		{ServerID: BLACK_HOLLOW_SERVER_ID, RoleID: "1459977200617914469"}, // BLACK HOLLOW server
 	}
 
 	// Role IDs for `Gold multiplier all`
 	GOLD_MULTIPLIER_RATE_ROLE_IDS = []RoleIDs{
-		{ServerID: PO_SERVER_ID, RoleID: "1408913674340663296"}, // P&O server
+		{ServerID: PO_SERVER_ID, RoleID: "1408913674340663296"},           // P&O server
+		{ServerID: BLACK_HOLLOW_SERVER_ID, RoleID: "1459977452381016196"}, // BLACK HOLLOW server
 	}
 	GOLD_MULTIPLIER_RATE_10X_ROLE_IDS = []RoleIDs{
-		{ServerID: PO_SERVER_ID, RoleID: "1408913719555129384"}, // P&O server
+		{ServerID: PO_SERVER_ID, RoleID: "1408913719555129384"},           // P&O server
+		{ServerID: BLACK_HOLLOW_SERVER_ID, RoleID: "1459977490905956373"}, // BLACK HOLLOW server
 	}
 
 	DB_SOC_RATE_ROLE_IDS = []RoleIDs{
-		{ServerID: PO_SERVER_ID, RoleID: "1408914346322690279"}, // P&O server
+		{ServerID: PO_SERVER_ID, RoleID: "1408914346322690279"},           // P&O server
+		{ServerID: BLACK_HOLLOW_SERVER_ID, RoleID: "1459977563089670289"}, // BLACK HOLLOW server
 	}
 	DB_SOC_RATE_10X_ROLE_IDS = []RoleIDs{
-		{ServerID: PO_SERVER_ID, RoleID: "1408914422407364759"}, // P&O server
+		{ServerID: PO_SERVER_ID, RoleID: "1408914422407364759"},           // P&O server
+		{ServerID: BLACK_HOLLOW_SERVER_ID, RoleID: "1459980853978464268"}, // BLACK HOLLOW server
 	}
 
 	MET_SOC_RATE_ROLE_IDS     = []RoleIDs{}
 	MET_SOC_RATE_10X_ROLE_IDS = []RoleIDs{}
 
 	LAB_BOSS_ROLE_IDS = []RoleIDs{
-		{ServerID: PO_SERVER_ID, RoleID: "1415136091681587201"},    // LAB_BOSS_SPAWN (P&O)
-		{ServerID: ROGUE_SERVER_ID, RoleID: "1392926180818292889"}, // ROGUE
+		{ServerID: PO_SERVER_ID, RoleID: "1415136091681587201"},           // LAB_BOSS_SPAWN (P&O)
+		{ServerID: ROGUE_SERVER_ID, RoleID: "1392926180818292889"},        // ROGUE
+		{ServerID: BLACK_HOLLOW_SERVER_ID, RoleID: "1459977614394654721"}, // BLACK HOLLOW
 	}
 
 	LAB_BOSS_GIBBON_ROLE = []RoleIDs{
-		{ServerID: PO_SERVER_ID, RoleID: "1419146940892975164"}, // LAB_BOSS_SPAWN (P&O)
+		{ServerID: PO_SERVER_ID, RoleID: "1419146940892975164"},           // LAB_BOSS_SPAWN (P&O)
+		{ServerID: BLACK_HOLLOW_SERVER_ID, RoleID: "1459977745923575991"}, // LAB_BOSS_SPAWN (BLACK HOLLOW)
 	}
 
 	LAB_BOSS_NAGA_LORD_ROLE = []RoleIDs{
-		{ServerID: PO_SERVER_ID, RoleID: "1419147201275494481"}, // LAB_BOSS_SPAWN (P&O)
+		{ServerID: PO_SERVER_ID, RoleID: "1419147201275494481"},           // LAB_BOSS_SPAWN (P&O)
+		{ServerID: BLACK_HOLLOW_SERVER_ID, RoleID: "1459977772784025630"}, // LAB_BOSS_SPAWN (BLACK HOLLOW)
 	}
 
 	LAB_BOSS_TALON_ROLE = []RoleIDs{
-		{ServerID: PO_SERVER_ID, RoleID: "1419148293853941760"}, // LAB_BOSS_SPAWN (P&O)
+		{ServerID: PO_SERVER_ID, RoleID: "1419148293853941760"},           // LAB_BOSS_SPAWN (P&O)
+		{ServerID: BLACK_HOLLOW_SERVER_ID, RoleID: "1459977810918637648"}, // LAB_BOSS_SPAWN (BLACK HOLLOW)
 	}
 
 	LAB_BOSS_HOWLER_ROLE = []RoleIDs{
-		{ServerID: PO_SERVER_ID, RoleID: "1419147397472194590"}, // LAB_BOSS_SPAWN (P&O)
+		{ServerID: PO_SERVER_ID, RoleID: "1419147397472194590"},           // LAB_BOSS_SPAWN (P&O)
+		{ServerID: BLACK_HOLLOW_SERVER_ID, RoleID: "1459977838093537310"}, // LAB_BOSS_SPAWN (BLACK HOLLOW)
 	}
 
 	CITY_WAR_CHANNEL_AND_MESSAGE_ID = []CityWarIDs{
 		{ChannelID: "1409693429482393710", MessageID: "1409701198692352151"}, // P&O Channel/Message
+		{ChannelID: "1459981658534187172", MessageID: "1459982183618969894"}, // BLACK HOLLOW Channel/Message
 	}
 
 	LAB_BOSS_CHANNEL_AND_MESSAGE_ID = []LabBosIDs{
